f007363721d17adece247129b80f1e62cafe8ec2
serhiichaiuk <chayck7@gmail.com>
2022-02-23 22:12:49 +0200

    emitting updates

---
+++ src/components/Curr.vue
@@ -0,0 +1,18 @@
+<template>
+  {{ dollar(amt) }}
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Curr',
+  props: ['amt'],
+  methods: {
+    dollar(value) {
+      return '$' + Number.parseFloat(value).toFixed(2)
+    }
+  }
+}
+</script>
+
+<style scoped></style>
