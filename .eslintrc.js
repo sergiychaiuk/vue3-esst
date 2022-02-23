ab4f2e596909c87ea87ed7f3482982c627344730
serhiichaiuk <chayck7@gmail.com>
2022-02-23 19:05:52 +0200

    mend

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,22 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "plugin:prettier/recommended",
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    'plugin:prettier/recommended'
   ],
   parserOptions: {
-    parser: "@babel/eslint-parser",
+    parser: '@babel/eslint-parser'
   },
   rules: {
-    "no-multi-spaces": ["error"],
-    "vue/no-multiple-template-root": "off",
-    "no-unused-vars": "off",
-    "no-use-before-define": "off",
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
-};
+    'no-multi-spaces': ['error'],
+    'vue/no-multiple-template-root': 'off',
+    'no-unused-vars': 'off',
+    'no-use-before-define': 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  }
+}
